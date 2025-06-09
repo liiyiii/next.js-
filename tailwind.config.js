489ea28d27065cdf9c34ac96e565b0bc84f6ffc2
liiyii <117975940+liiyiii@users.noreply.github.com>
2025-06-09 16:46:26 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,16 @@ module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", 
   ],
   theme: {
-    extend: {}, // Temporarily empty for testing
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
 };
