70065a38c42366b0964ff9c09e6ee6c9eb37adf3
liiyii <117975940+liiyiii@users.noreply.github.com>
2025-06-19 14:21:04 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/**', // Use a very broad pathname for testing
+      },
+    ],
+  },
   /* config options here */
 };
 
